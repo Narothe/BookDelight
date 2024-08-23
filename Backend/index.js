148def17cf518ce71d5501aa611b946fe04d20d5
Narothe <kuba.dojka@gmail.com>
2024-08-23 17:39:27 +0200

    feat: backend: connect to database

--- Backend/index.js
+++ Backend/index.js
@@ -2,10 +2,25 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
+});
+
+const { Client } = require('pg')
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+})
+client.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to database!");
 });
