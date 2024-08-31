731fbadffd38e354c50b16b0607f7ec35a2fca4c
Narothe <kuba.dojka@gmail.com>
2024-08-31 21:09:25 +0200

    feat: prepare to database update
    add 6 new tables

--- Backend/routes/register.js
+++ Backend/routes/register.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { register } = require('../controllers/registerController');
+const { register } = require('../controllers/createController');
 
 /**
  * @swagger
