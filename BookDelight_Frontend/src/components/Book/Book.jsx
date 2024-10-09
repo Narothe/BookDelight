ad3c2bf11763563a2e18159c0cfa6e9194ddec74
Narothe <kuba.dojka@gmail.com>
2024-10-09 22:08:35 +0200

    feat: (frontend) start endpoint management from a different path

--- BookDelight_Frontend/src/components/Book/Book.jsx
+++ BookDelight_Frontend/src/components/Book/Book.jsx
@@ -7,23 +7,35 @@ import LinkButton from "../../utils/LinkButton";
 
 function Book() {
     const {id} = useParams();
-    const query = `${process.env.REACT_APP_BACKEND_URL}/book/${id}`;
+    const bookQuery = `${process.env.REACT_APP_BACKEND_URL}/book/${id}`;
+    const reviewQuery = `${process.env.REACT_APP_BACKEND_URL}/book/${id}/reviews`;
     const photoUrl = `${process.env.REACT_APP_PHOTO_URL}`;
 
     const [book, setBook] = useState(null);
+    const [review, setReview] = useState(null);
 
     useEffect(() => {
         const fetchBookDetails = async () => {
-
             try {
-                const response = await axios.get(query);
+                const response = await axios.get(bookQuery);
                 setBook(response.data);
             } catch (err) {
                 console.error('Error fetching book details:', err);
             }
         };
 
+        const fetchReviewsDetails = async () => {
+            try {
+                const response = await axios.get(reviewQuery);
+                setReview(response.data);
+                console.log(response.data);
+            } catch (err) {
+                console.error('Error fetching review details:', err);
+            }
+        };
+
         fetchBookDetails();
+        fetchReviewsDetails();
     }, [id]);
 
     if (!book) return <p>No book data found</p>;
