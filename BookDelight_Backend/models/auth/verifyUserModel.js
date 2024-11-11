e76c00274261ff6d9c5e05708dcc436641f6b8b4
Narothe <kuba.dojka@gmail.com>
2024-11-11 23:01:28 +0100

    refactor: diagrams photos, save verify user - change in database

--- BookDelight_Backend/models/auth/verifyUserModel.js
+++ BookDelight_Backend/models/auth/verifyUserModel.js
@@ -1,12 +1,19 @@
 const pool = require('../../config/db');
 
 const checkIfUserIsVerified = async (userId) => {
-    const query = 'SELECT verify FROM bookdelight.Verify_User WHERE id_user = $1';
+    const query = 'SELECT verify FROM bookdelight.Users WHERE id_user = $1';
     const values = [userId];
 
     try {
         const result = await pool.query(query, values);
-        return result.rows.length > 0;
+
+        if (result.rows[0].verify === true) {
+            return { error: 'An error occurred while checking if the user is verified.' };
+        } else if (result.rows[0].verify === false) {
+            return false;
+        }
+
+        // return result.rows.length > 0;
     } catch (err) {
         console.error('Error while checking if the user is verified:', err);
         return {error: 'An error occurred during checking if the user is verified.'};
@@ -14,9 +21,11 @@ const checkIfUserIsVerified = async (userId) => {
 }
 
 const verifyUser = async (userId) => {
+    console.log('Verifying user:', userId);
     const query = `
-        INSERT INTO bookdelight.Verify_User(id_user, verify)
-        VALUES ($1, $2)
+        UPDATE bookdelight.Users
+        SET verify = $2
+        WHERE id_user = $1
             RETURNING id_user;
         `;
 
@@ -24,6 +33,7 @@ const verifyUser = async (userId) => {
 
     try {
         const result = await pool.query(query, values);
+        console.log('User verified:', result.rows[0].id_user);
         return result.rows[0].id_user;
     } catch (err) {
         console.error('Error while verifying the user:', err);
