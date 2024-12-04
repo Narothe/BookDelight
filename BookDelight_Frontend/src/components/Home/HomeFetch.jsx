228ff46243ef10f4fea205ec9fbf8407cd916b37
Narothe <kuba.dojka@gmail.com>
2024-12-04 21:22:42 +0100

    feat: (frontend): home slider

--- BookDelight_Frontend/src/components/Home/HomeFetch.jsx
+++ BookDelight_Frontend/src/components/Home/HomeFetch.jsx
@@ -1,12 +1,12 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
-import LinkButton from "../../utils/LinkButton";
 import HomeFetchDesktop from "./HomeFetchDesktop";
 import HomeFetchMobile from "./HomeFetchMobile";
 import {Link} from "react-router-dom";
-import DisplayRating from "../../utils/DisplayRating";
 import HomeFetchBlankDesktop from "./HomeFetchBlankDesktop";
 import HomeFetchBlankMobile from "./HomeFetchBlankMobile";
+
+import HomeSlider from "../../utils/HomeSlider";
 
 function HomeFetch() {
 
@@ -32,6 +32,9 @@ function HomeFetch() {
         <div className="py-8">
             {data.length > 0 ? (
                 <div>
+                    <div className="pb-6">
+                        <HomeSlider books={data} photoUrl={photoUrl} />
+                    </div>
                     <div className="hidden md:block">
                         <div
                             className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 border flex-wrap gap-2 pt-4 p-2 mb-4 rounded-md shadow-md bg-custom-new-white">
@@ -39,7 +42,6 @@ function HomeFetch() {
                                 <div key={item.id_book}
                                      className="flex flex-col justify-between border pt-4 px-4 pb-2 mb-4 rounded-md shadow-md bg-white flex-grow">
                                     <Link to={`/book/${item.id_book}`}>
-
                                         <HomeFetchDesktop item={item} photoUrl={photoUrl}/>
                                         <div className="relative">
                                             <div className="lg:mt-6">
---
+++ BookDelight_Frontend/src/utils/HomeSlider.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination, Autoplay } from 'swiper/modules';
+import {Link} from "react-router-dom";
+
+function HomeSlider({ books, photoUrl }) {
+    const booksWithCovers = books.filter((book) => book.photo_path);
+
+    return (
+        <div className="w-full border rounded-md shadow-md bg-custom-new-white p-4">
+            <h2 className="text-2xl font-semibold mb-4 text-center">Perhaps this is what you are looking for?</h2>
+            {booksWithCovers.length > 0 ? (
+                <Swiper
+                    modules={[Navigation, Pagination, Autoplay]}
+                    spaceBetween={20}
+                    slidesPerView={3}
+                    navigation
+                    pagination={{ clickable: true }}
+                    autoplay={{ delay: 3000 }}
+                    breakpoints={{
+                        1: { slidesPerView: 1 },
+                        670: { slidesPerView: 2 },
+                        1024: { slidesPerView: 3 },
+                        1280: { slidesPerView: 4 },
+                        1600: { slidesPerView: 5 },
+                    }}
+                >
+                    {booksWithCovers.map((book) => (
+                        <SwiperSlide key={book.id} className="flex justify-center">
+                            <Link to={`/book/${book.id_book}`}>
+                                <div className="bg-white shadow-md rounded-md p-4 mb-4 w-72 ">
+                                    <img
+                                        src={`${photoUrl}${book.photo_path}`}
+                                        alt={book.title}
+                                        className="w-full h-40 object-cover rounded-md mb-4"
+                                    />
+                                    <h3 className="text-lg font-semibold">{book.title}</h3>
+                                    <p className="text-sm text-gray-600 mt-2">
+                                        {book.authors.join(', ')}
+                                    </p>
+                                </div>
+                            </Link>
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
+            ) : (
+                <p className="text-center text-gray-500">No books with covers available.</p>
+            )}
+        </div>
+    );
+}
+
+export default HomeSlider;
