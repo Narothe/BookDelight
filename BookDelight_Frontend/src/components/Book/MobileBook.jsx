3093742e73914a655cf26d67eb8ef91e731ca2f9
Narothe <kuba.dojka@gmail.com>
2025-01-03 00:28:15 +0100

    feat: (frontend): mobile version of book looks better

--- BookDelight_Frontend/src/components/Book/MobileBook.jsx
+++ BookDelight_Frontend/src/components/Book/MobileBook.jsx
@@ -12,51 +12,57 @@ function DesktopBook({ book, review, photoUrl }) {
     const userPhotoUrl = `${process.env.REACT_APP_USER_PHOTO_URL}`;
 
     return (
-        <div className="flex flex-col">
-
-            <div className="flex border p-4 rounded-md shadow-md bg-white">
-                <div className="flex flex-col w-56">
-                    <div className="pb-4">
-                        <LoadBookImage item={book} photoUrl={photoUrl}/>
-                    </div>
-                    <div className="flex px-4 mb-2">
-                        <BookRatingInfo book={book}/>
-                    </div>
-                    <BookInfo book={book}/>
-                </div>
-                <div className="flex flex-col w-full">
-                    <div className="flex flex-row w-full px-6 justify-between">
-                        <div className="flex flex-col">
-                            <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold">{book.title}</h2>
-                            <p className="text-sm md:text-base lg:text-lg font-semibold"> by:{' '} {book.authors.join(', ')} </p>
-                            <p className="text-sm md:text-base lg:text-lg font-semibold mb-2"> Genres:{' '} {book.genres.join(', ')} </p>
+        <div>
+            <div className="flex flex-col border p-4 rounded-md shadow-md bg-white">
+                <div className="flex">
+                    <div className="flex flex-col w-56">
+                        <div className="pb-4">
+                            <LoadBookImage item={book} photoUrl={photoUrl}/>
                         </div>
-                    </div>
-                    <div className="flex flex-col h-full justify-between">
-                        <div className="flex flex-col text-sm md:text-base lg:text-lg font-semibold px-6">
-                            <p className="py-4 mb-2">{book.short_description}</p>
-                            <p className="py-2 mb-2">{book.long_description}</p>
+                        <div className="flex px-4 mb-2">
+                            <BookRatingInfo book={book}/>
                         </div>
-                        <div className="ml-4 p-2">
-                            <Link to={`/user/${book.id_user}`}>
-                                <div className="flex flex-row items-center">
-                                    <p className="text-xs md:text-sm lg:text-base mr-1"> Book added by: {' '}
-                                        <strong> {book.book_added_by_username} </strong></p>
-                                    <div className="w-6">
-                                        <LoadBookUserImage item={book} photoUrl={userPhotoUrl}/>
+                        <BookInfo book={book}/>
+                    </div>
+                    <div className="flex flex-col w-full">
+                        <div className="flex flex-row w-full px-6 justify-between">
+                            <div className="flex flex-col">
+                                <h2 className="text-xl md:text-2xl lg:text-3xl font-semibold">{book.title}</h2>
+                                <p className="text-sm md:text-base lg:text-lg font-semibold mb-2"> by:{' '} {book.authors.join(', ')} </p>
+                                <p className="text-sm md:text-base lg:text-lg font-semibold mb-2"> Genres:{' '} {book.genres.join(', ')} </p>
+                            </div>
+                        </div>
+                        <div className="flex flex-col h-full">
+                            <div className="flex flex-col text-sm md:text-base lg:text-lg font-semibold px-6">
+                                <p className="py-4 mb-2">{book.short_description}</p>
+                            </div>
+                            <div className="ml-4 p-2">
+                                <Link to={`/user/${book.id_user}`}>
+                                    <div className="flex flex-col">
+                                        <p className="text-xs md:text-sm lg:text-base mr-1"> Book added by: {' '}</p>
+                                        <div className="flex flex-row">
+                                            <p className="text-xs md:text-sm lg:text-base"><strong> {book.book_added_by_username} </strong></p>
+                                            <div className="w-6 ml-1">
+                                                <LoadBookUserImage item={book} photoUrl={userPhotoUrl}/>
+                                            </div>
+                                        </div>
                                     </div>
-                                </div>
-                            </Link>
+                                </Link>
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div className="text-sm md:text-base lg:text-lg font-semibold">
+                    <p className="py-4 mb-2">{book.long_description}</p>
                 </div>
             </div>
+
             <div className="flex flex-col border mt-4 p-4 rounded-md shadow-md bg-white">
                 <div className="flex justify-between">
                     <h1 className="text-2xl lg:text-3xl font-bold mb-4 font-mono">Reviews</h1>
                     <AddReview bookId={book.id_book} bookTitle={book.title}/>
                 </div>
-                    {review && (
+                {review && (
                     <ReviewMobile review={review}/>
                 )
                 }
