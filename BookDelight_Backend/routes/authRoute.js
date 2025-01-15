198b531e4833bc510c2fdcaa6418633637e58637
Kuba <kuba.dojka@gmail.com>
2025-01-15 23:52:10 +0100

    feat: (swagger): authRoute

--- BookDelight_Backend/routes/authRoute.js
+++ BookDelight_Backend/routes/authRoute.js
@@ -13,164 +13,348 @@ router.get('/user/is-verified', authenticateToken, getVerified);
 
 module.exports = router;
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Log in to the application
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               identity:
+ *                 type: string
+ *                 example: john_doe
+ *               password:
+ *                 type: string
+ *                 example: P@ssw0rd!
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: User logged in successfully
+ *                 token:
+ *                   type: string
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...
+ *                 user:
+ *                   type: object
+ *                   properties:
+ *                     userId:
+ *                       type: integer
+ *                       example: 1
+ *                     email:
+ *                       type: string
+ *                       example: user@example.com
+ *                     username:
+ *                       type: string
+ *                       example: john_doe
+ *                     isAdmin:
+ *                       type: boolean
+ *                       example: false
+ *       400:
+ *         description: Missing email/username or password
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Email or username and password are required.
+ *       401:
+ *         description: Invalid credentials
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: Invalid email or username.
+ *                 message:
+ *                   type: string
+ *                   example: Detailed error message (optional)
+ *       500:
+ *         description: Server error during login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: An error occurred during adding the session.
+ */
 
+/**
+ * @swagger
+ * /logout:
+ *   post:
+ *     summary: Log out from the application
+ *     tags:
+ *       - Authentication
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User logged out successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: string
+ *                   example: User logged out successfully.
+ *       401:
+ *         description: No token provided
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: No token provided.
+ *       500:
+ *         description: Server error during logout
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: An error occurred during logging out the user.
+ */
 
-// /**
-//  * @swagger
-//  * components:
-//  *   schemas:
-//  *     Login:
-//  *       type: object
-//  *       required:
-//  *         - identity
-//  *         - password
-//  *       properties:
-//  *         identity:
-//  *           type: string
-//  *           description: The user's email
-//  *         password:
-//  *           type: string
-//  *           description: The user's password
-//  *       example:
-//  *         identity: user@example.com
-//  *         password: SecurePassword123
-//  */
-//
-// /**
-//  * @swagger
-//  * /login:
-//  *   post:
-//  *     summary: Login a user
-//  *     tags: [Auth]
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         application/json:
-//  *           schema:
-//  *             $ref: '#/components/schemas/Login'
-//  *     responses:
-//  *       200:
-//  *         description: User logged in successfully.
-//  *       400:
-//  *         description: Email and password are required.
-//  *       401:
-//  *         description: Invalid password or email.
-//  *       500:
-//  *         description: An error occurred during logging in the user.
-//  */
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 example: john.doe@example.com
+ *               password:
+ *                 type: string
+ *                 example: P@ssw0rd!
+ *               username:
+ *                 type: string
+ *                 example: john_doe
+ *               firstName:
+ *                 type: string
+ *                 example: John
+ *               lastName:
+ *                 type: string
+ *                 example: Doe
+ *               birthday:
+ *                 type: date
+ *                 example: 19-02-1990
+ *
+ *
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: User registered successfully.
+ *                     userId:
+ *                       type: integer
+ *                       example: 1
+ *       400:
+ *         description: Bad request - validation errors or missing fields
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: All fields are required.
+ *       500:
+ *         description: Server error during registration
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: An error occurred while registering the user.
+ */
 
+/**
+ * @swagger
+ * /user/verify:
+ *   post:
+ *     summary: Send a verification email to the user
+ *     tags:
+ *       - User Verification
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               userId:
+ *                 type: integer
+ *                 example: 1
+ *     responses:
+ *       200:
+ *         description: Verification email sent successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: Verification email sent.
+ *       400:
+ *         description: User is already verified
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: User is already verified.
+ *       500:
+ *         description: Internal server error during email verification
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: An error occurred while sending the verification email.
+ */
 
+/**
+ * @swagger
+ * /verify-email:
+ *   get:
+ *     summary: Verify a user's email with a token
+ *     tags:
+ *       - User Verification
+ *     parameters:
+ *       - in: query
+ *         name: token
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: Verification token
+ *     responses:
+ *       200:
+ *         description: User verified successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: object
+ *                   properties:
+ *                     message:
+ *                       type: string
+ *                       example: User verified successfully.
+ *       400:
+ *         description: Verification failed
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: User cannot be marked as Verified.
+ *       500:
+ *         description: Internal server error during verification
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: An error occurred while verifying the user.
+ */
 
-// /**
-//  * @swagger
-//  * components:
-//  *   securitySchemes:
-//  *     bearerAuth:
-//  *       type: http
-//  *       scheme: bearer
-//  *       bearerFormat: JWT
-//  *
-//  *   schemas:
-//  *     Logout:
-//  *       type: object
-//  */
-//
-// /**
-//  * @swagger
-//  * /logout:
-//  *   post:
-//  *     summary: Logout a user
-//  *     tags: [Auth]
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         application/json:
-//  *           schema:
-//  *             $ref: '#/components/schemas/Logout'
-//  *     responses:
-//  *       200:
-//  *         description: User logged out successfully.
-//  *       401:
-//  *         description: Token is required.
-//  *       403:
-//  *         description: Session expired.
-//  *       500:
-//  *         description: An error occurred during logging out the user.
-//  *
-//  */
-
-
-
-
-
-// /**
-//  * @swagger
-//  * components:
-//  *   schemas:
-//  *     Register:
-//  *       type: object
-//  *       required:
-//  *         - email
-//  *         - password
-//  *         - username
-//  *         - firstName
-//  *         - lastName
-//  *         - birthDay
-//  *         - birthMonth
-//  *         - birthYear
-//  *       properties:
-//  *         email:
-//  *           type: string
-//  *           description: The user's email (must be unique)
-//  *         password:
-//  *           type: string
-//  *           description: The user's password
-//  *         username:
-//  *           type: string
-//  *           description: The user's username (must be unique)
-//  *         firstName:
-//  *           type: string
-//  *           description: The user's first name
-//  *         lastName:
-//  *           type: string
-//  *           description: The user's last name
-//  *         birthDay:
-//  *           type: integer
-//  *           description: The user's birth day
-//  *         birthMonth:
-//  *           type: integer
-//  *           description: The user's birth month
-//  *         birthYear:
-//  *           type: integer
-//  *           description: The user's birth year. Year must be number bigger than 1800
-//  *       example:
-//  *         email: user@example.com
-//  *         password: SecurePassword123
-//  *         username: username123
-//  *         firstName: John
-//  *         lastName: Doe
-//  *         birthDay: 15
-//  *         birthMonth: 8
-//  *         birthYear: 1990
-//  */
-//
-// /**
-//  * @swagger
-//  * /register:
-//  *   post:
-//  *     summary: Register a new user
-//  *     tags: [Auth]
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         application/json:
-//  *           schema:
-//  *           $ref: '#/components/schemas/Register'
-//  *     responses:
-//  *       201:
-//  *         description: User registered successfully
-//  *       400:
-//  *         description: Bad request
-//  *       500:
-//  *         description: Internal server error
-//  */
+/**
+ * @swagger
+ * /user/is-verified:
+ *   get:
+ *     summary: Check if the user is verified
+ *     tags:
+ *       - User Verification
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: User verification status
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 result:
+ *                   type: object
+ *                   example: true
+ *       404:
+ *         description: User or verification status not found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: User not found.
+ *       500:
+ *         description: Internal server error while checking user verification status
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   example: An error occurred while getting the user.
+ */
